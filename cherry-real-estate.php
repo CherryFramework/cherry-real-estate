b7ad2e2d141dbf6895b126602681939eb2cea984
dkfiresky <dkholodilin@live.com>
2016-09-20 17:12:10 +0300

    ADD: capabilities for feature, tag and type

--- cherry-real-estate.php
+++ cherry-real-estate.php
@@ -476,6 +476,12 @@ if ( ! class_exists( 'Cherry_Real_Estate' ) ) {
 				"edit_others_{$capability_type}s"   => true,
 				"publish_{$capability_type}s"       => true,
 				"read_private_{$capability_type}s"  => true,
+				"read_{$capability_type}_type"      => true,
+				"edit_{$capability_type}_type"      => true,
+				"read_{$capability_type}_tag"       => true,
+				"edit_{$capability_type}_tag"       => true,
+				"read_{$capability_type}_feature"   => true,
+				"edit_{$capability_type}_feature"   => true,
 			) );
 			$capabilities = apply_filters( 'cherry_re_custom_property_capabilities', $capabilities );
 
@@ -512,12 +518,12 @@ if ( ! class_exists( 'Cherry_Real_Estate' ) ) {
 				"edit_private_{$capability_type}s"     => true,
 				"edit_published_{$capability_type}s"   => true,
 				"edit_{$capability_type}s"             => true,
-				"read_{$capability_type}s_type"        => true,
-				"edit_{$capability_type}s_type"        => true,
-				"read_{$capability_type}s_tag"         => true,
-				"edit_{$capability_type}s_tag"         => true,
-				"read_{$capability_type}s_feature"     => true,
-				"edit_{$capability_type}s_feature"     => true,
+				"read_{$capability_type}_type"        => true,
+				"edit_{$capability_type}_type"        => true,
+				"read_{$capability_type}_tag"         => true,
+				"edit_{$capability_type}_tag"         => true,
+				"read_{$capability_type}_feature"     => true,
+				"edit_{$capability_type}_feature"     => true,
 				'edit_posts'                           => true, // `save_post` action-callback check `edit_posts` capability.
 				'read'                                 => true,
 				'upload_files'                         => true,
@@ -539,6 +545,9 @@ if ( ! class_exists( 'Cherry_Real_Estate' ) ) {
 				'edit_posts'                         => true, // `save_post` action-callback check `edit_posts` capability.
 				'read'                               => true,
 				'upload_files'                       => true,
+				"read_{$capability_type}_type"        => true,
+				"read_{$capability_type}_tag"         => true,
+				"read_{$capability_type}_feature"     => true,
 			) );
 		}
 
