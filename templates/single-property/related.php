0f282bfd499f8dd80c5e2757d434502fbb91a9c3
cheh <chehovskiy.dima@gmail.com>
2016-11-07 18:06:27 +0200

    FIX: related property layout

--- templates/single-property/related.php
+++ templates/single-property/related.php
@@ -16,29 +16,29 @@ $heading = esc_html( apply_filters(
 	esc_html__( 'Related Properties', 'cherry-real-estate' )
 ) );
 
-$data = Cherry_RE_Property_Data::get_instance();
-
-$meta_key   = cherry_real_estate()->get_meta_prefix() . 'status';
-$meta_query = array(
+$property_id = get_the_ID();
+$meta_key    = cherry_real_estate()->get_meta_prefix() . 'status';
+$meta_query  = array(
 	array(
 		'key'     => $meta_key,
-		'value'   => get_post_meta( get_the_ID(), $meta_key, true ),
+		'value'   => get_post_meta( $property_id, $meta_key, true ),
 		'compare' => '=',
 	),
 );
 
 $query_args = apply_filters( 'cherry_re_property_related_args', array(
-	'post__not_in'        => array( get_the_ID() ),
+	'author'              => get_the_author_meta( 'ID' ),
+	'post__not_in'        => array( $property_id ),
 	'meta_query'          => $meta_query,
 	'ignore_sticky_posts' => true,
-	'number'              => 3,
+	'number'              => 2,
 	'echo'                => false,
 	'item_class'          => 'tm-property-related__item',
-	'author'              => get_the_author_meta( 'ID' ),
-	'css_class'           => 'tm-property-related__wrap tm-property__wrap--related',
+	'css_class'           => 'tm-property-items tm-property-related__wrap tm-property__wrap--related',
 	'template'            => 'related.tmpl',
 ) );
 
+$data    = Cherry_RE_Property_Data::get_instance();
 $related = $data->the_property( $query_args );
 
 if ( ! $related ) {
